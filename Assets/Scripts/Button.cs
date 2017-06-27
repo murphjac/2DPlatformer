050683d78566451a8db175c05254daa26fe24d04
Jacob Murphy <murphyj.1990@gmail.com>
2017-06-27 11:53:51 -0700

    Door as Moving Platform
    Reusing the MovingPlatform class for the door makes more sense, as it
    solves the player displacement issue and will allow for me to extend the
    features of that class generally. In that sense, there is now a generic
    Button class which currently activates/deactivates a target
    MovingPlatform.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class Button : MonoBehaviour, UsableObject {
+
+    public PlatformController targetPlatform;
+
+    private Renderer rend;
+    
+    void Start () {
+        rend = GetComponent<Renderer>();
+        SetButtonColor();
+    }
+
+    void SetButtonColor()
+    {
+        string materialPath = targetPlatform.active ? "Materials/Player_Wind" : "Materials/Player_Fire";
+        rend.sharedMaterial = Resources.Load(materialPath) as Material;
+    }
+
+    public void Use()
+    {
+        targetPlatform.active = !targetPlatform.active;
+        SetButtonColor();
+    }
+}
